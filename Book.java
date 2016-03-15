5e45420a395bf44b36306eae2745e6471fdff33d
jgreen12 <jgreen12@islander.tamucc.edu>
2016-03-15 17:17:08 -0500

    Update Book.java

--- Book.java
+++ Book.java
@@ -18,13 +18,7 @@ public class Book {
     public String pDate;
     public String pub;
     public String category;
-    
-    
-    
-    
-    
-    
-    
+    //constructor
     public Book(String tempId, String tempIsbn, int tempInv, String tempTitle, String tempAuthor, String tempDate, String tempPub, String tempCat){
         bID = tempId;
         ISBN = tempIsbn;
@@ -35,18 +29,17 @@ public class Book {
         pub = tempPub;
         category = tempCat;
     }
-    
+    //checkout function
     public void checkOut(){
+        
         if(invAmnt > 0){
             invAmnt--;
         }
     }
-    
+    //overridden toString
     @Override
     public String toString()
     {
         return bID + ", " + ISBN + ", " + invAmnt+ ", " + title + ", "+ author + ", "+ pDate + ", "+ pub + ", "+ category;
-    }
-    
-    
+    }        
 }
