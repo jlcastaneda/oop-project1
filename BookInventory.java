5de29a43e105611add31e5763f5ea18b1a2a5925
jgreen12 <jgreen12@islander.tamucc.edu>
2016-03-16 18:19:20 -0500

    Update BookInventory.java

--- BookInventory.java
+++ BookInventory.java
@@ -8,6 +8,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.Reader;
+import java.util.Scanner;
 
 /**
  *
@@ -15,9 +16,16 @@ import java.io.Reader;
  */
 public class BookInventory {
     //create array list
-    public List<Book> invItem = new ArrayList<>();
+    public List<Book> invItem = new ArrayList<>();  //list of objects   
+    Scanner s = new Scanner(System.in); //scanner for input
+    String uName;   //username
     
-    public void readInventory(){
+    
+    public BookInventory(String uName){
+        this.uName = uName;
+    }
+    
+    public void readInventory(){    //reads inventory from file
 
         try{
             //create buff read with an instance of file read
@@ -60,13 +68,15 @@ public class BookInventory {
             ioe.printStackTrace();
         }
     }
-    public void printInventory(){
+    public void printFullInventory(){   //prints the full inventory
+        int x = 0;
 	for(Book each: invItem){
-		System.out.println(each);	
+            System.out.println(x+": ");
+            System.out.println(each);	
+            x++;
 	}
     }
-    public void writeInventory(){
-        
+    public void writeInventory(){        //writes inventory to a file
 	try(Writer writer = new BufferedWriter((new OutputStreamWriter(new FileOutputStream("BookInventory.txt"), "utf-8")))){
             
             for(Book each: invItem){
@@ -79,5 +89,135 @@ public class BookInventory {
             
         }
     }
+    
+    
+    
+    
+    
+    public void printCatInventory(int h, int l){
+        int item;
+        System.out.println("Category: "+invItem.get(l).category);   //prints book in category
+	for(int x = l; x <= h; x++){
+
+            System.out.println(x+": "+invItem.get(x));	
+	}
+        int choice;
+        System.out.println("Check out an item?");   //checks out item
+        System.out.println("1: Yes      2: No");
+        choice = s.nextInt();
+        if(choice == 1){
+            do{
+                System.out.println("Input the item number: ");
+                item = s.nextInt();
+                if(item < l || item > h){
+                    System.out.println("Item number not valid, attempt again.");
+                }
+            }while(item < l || item > h);
+            //checkout
+            checkOut(item);
+        }
+        else{
+            System.out.println("Returning to menu.");
+        }
+    }
+    
+    public void browse(){
+        readInventory();    //reads inventory
+        int choice = 0; //selection
+        int high;   //high range
+        int low;    //low range
+        System.out.println("Please select a category: ");
+        System.out.println("1: Art");
+        System.out.println("2: Business & Economics");
+        System.out.println("3: Computer Science");
+        System.out.println("4: Design");
+        System.out.println("5: Education");
+        System.out.println("6: Law");
+        System.out.println("7: Mathematics");
+        System.out.println("8: Music");
+        System.out.println("9: Philosophy & Psychology");
+        choice = s.nextInt();
+        high = (choice * 5) - 1;    //calc's high value
+        low = high - 4; //calcs low value
+        printCatInventory(high, low);   //prints books in those ranges
+    }
+    
+    
+    
+    
+    
+    //searches inventory based off of Book ID
+    public void searchBookID(){
+        readInventory();    //reads inventory
+        Scanner s = new Scanner(System.in);
+        String bID = null;
+        int found = 0;
+        int count = 0;
+        int y;
+        System.out.println("1. Search for item");   //menu
+        System.out.println("2. Exit");
+        y = s.nextInt();    //selection
+        while(y == 1){
+            
+            System.out.println("Please enter a book ID: ");
+            bID = s.nextLine(); //gives book id
+            for(Book each: invItem){    //searches through each item in the list
+                if(bID.equals(invItem.get(count).bID)){
+                    found = 1;
+                }
+                else{
+                    count++;
+                }
+            }
+            if(found == 1){ //if found
+                //print item
+            
+                int x = 0;
+                System.out.println("Item Found: "+invItem.get(count));  //print book info
+                if(invItem.get(count).invAmnt > 0){
+                    System.out.println("Check out this item?"); //asks if they want to check out book
+                    System.out.println("1: Yes      2: No");
+                    x = s.nextInt();
+                    if(x == 1){
+                        //check out process
+                        checkOut(count);
+                    }
+                    else{
+                        System.out.println("Moving back to menu."); 
+                    }
+                }
+                else{
+                    System.out.println("Sorry, this item is currently unavailable. Moving to menu.");
+                }
+            }
+            else{
+                System.out.println("Item not found with entered ID, moving to menu.");
+            }
+            System.out.println("1. Search For Item");
+            System.out.println("2. Exit Menu");
+        }
+    }
+    
+    
+    public void checkOut(int invNum){
+        //Inventory controller       
+        //most operations require the username of the user                
+        //get inventory and print                                              
+        History h = new History(uName);                        
+        //checkoutprocess
+        invItem.get(invNum).checkOut();
+        h.checkOut(invItem.get(invNum));        
+        //Writes Inventory
+        writeInventory();      
+        System.out.println("Check out complete, inventory modified, user history updated.");
+        System.out.println("Date and Time of checkout: ");
+        System.out.println("Date and Time of return: ");
+        System.out.println("Returning to menu.");
+        //print the user's History
+    }
+    
+    
+    
+    
     
 }
