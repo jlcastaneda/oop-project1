c6960aae51275315a69e5f1a33f2bbe922418886
jgreen12 <jgreen12@islander.tamucc.edu>
2016-03-16 18:19:52 -0500

    Update BookInventory.java

--- BookInventory.java
+++ BookInventory.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package oopproj1;
+package oop_bookmart;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
