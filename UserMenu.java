342c79166ebe6fb477bf9c72a3c5b5993902bfeb
jgreen12 <jgreen12@islander.tamucc.edu>
2016-03-17 21:23:15 -0500

    Update UserMenu.java

--- UserMenu.java
+++ UserMenu.java
@@ -6,12 +6,15 @@ package oop_bookmart;
 import java.util.Scanner;
 
 public class UserMenu {
-	
+	int student;
+	int staff;
 	boolean exit;
 	//Student Student1 = new Student();
 	String uName;
-	public void displayMenu(String uName)
+	public void displayMenu(String uName, int student, int staff)
 	{
+		this.student = student;
+		this.staff = staff;
 		this.uName = uName;
 		while(!exit)
 		{
@@ -24,13 +27,14 @@ public class UserMenu {
 	
 	private void printMenu()
 	{
-		System.out.println("User Menu");
-		System.out.println("1. View User History");
-		System.out.println("2. Search by BookID");
-		System.out.println("3. View book inventory");
-                System.out.println("4. Browse by Category");
-		System.out.println("5. Show student");
-		System.out.println("0. Exit");
+            System.out.println("User Menu");
+            System.out.println("1. View User History");	
+            System.out.println("2. Search by BookID");
+            System.out.println("3. View book inventory");
+            System.out.println("4. Browse by Category");
+            System.out.println("5. Return a Book");
+            System.out.println("6. Show Info");
+            System.out.println("0. Exit");
 	}
 
 	private int getInput()
@@ -39,7 +43,7 @@ public class UserMenu {
 		Scanner selection = new Scanner(System.in);
 		int choice = -9;
 		//System.out.println("Entering while loop\n");
-		while (choice < 0 || choice > 4)
+		while (choice < 0 || choice > 7)
 		{
 			//System.out.println("in while loop");
 			System.out.print("Enter selection: ");
@@ -82,12 +86,17 @@ public class UserMenu {
 			break;
                         
                 case 4:
-                        //browse by category
-                        System.out.println("Browse by category\n");
-                        browseCat();
-                        break;
+                    //browse by category
+                    System.out.println("Browse by category\n");
+                    browseCat();
+                    break;
 			
 		case 5:
+			//return a book
+			System.out.println("Return a book\n");
+			returnBook();
+			
+		case 6:
 			showStudent();
 		
 		default:
@@ -100,27 +109,44 @@ public class UserMenu {
 	
 	public void viewHistory()
 	{
-            History h = new History(uName);
-            h.printHistory();
+        History h = new History(uName);
+        h.printHistory();
 	}
 	
 	public void searchID()
 	{
-            BookInventory BI = new BookInventory(uName);            
-            BI.searchBookID();
-            BI.writeInventory();
+        BookInventory BI = new BookInventory(uName, student, staff);
+        String b = null;
+        BI.searchBookID(b);
+        //BI.writeInventory();
 	}
 	
 	public void viewBookInv()
 	{
-            BookInventory BI = new BookInventory(uName);
+            BookInventory BI = new BookInventory(uName, student, staff);
             BI.printFullInventory();
 	}
-        public void browseCat(){
-            BookInventory BI = new BookInventory(uName);
-            BI.browse();
+    public void browseCat(){
+        BookInventory BI = new BookInventory(uName, student, staff);
+        BI.browse();
             
+    }
+	
+    public void returnBook(){
+        //create a checked book object reading from file, 
+        CheckedOut CO = new CheckedOut(uName, student, staff);
+        String ID = CO.returnProcess();
+        if(ID != "N"){
+            BookInventory b = new BookInventory(uName, student, staff);
+            b.returnBook(ID);
+	//return book, overwrite file
         }
+        else{
+            System.out.println("You have no items to return.");
+        }
+           
+		
+	}
 	
 	public void showStudent()
 	{
